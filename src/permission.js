0a2781b8fc9706c34680609b0b13c58513ba1f09
Jackie <16275507@qq.com>
2019-08-23 17:42:26 +0800

    增加权限控制

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,66 @@
 import router from './router'
+import store from './store'
+import { getToken } from '@/utils/auth' // get token from localStorage
+import defaultSettings from '@/config/index.js'
+
+const getPageTitle = (pageTitle) => {
+  return pageTitle ? `${pageTitle} - ${defaultSettings.title || 'vue vant template'}` : `${defaultSettings.title || 'vue vant template'}`
+}
+
+const whiteLists = ['/login'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
-  next()
+  // set page title
+  document.title = getPageTitle(to.meta.title)
+
+  // determine whether the user has logged in
+  const hasToken = getToken()
+
+  if (hasToken) {
+    if (to.path === '/login') {
+      // if is logged in, redirect to the home page
+      next({ path: '/' })
+    } else {
+      // determine whether the user has obtained his permission roles through getInfo
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      if (hasRoles) {
+        next()
+      } else {
+        try {
+          // get user info
+          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+          const { roles } = await store.dispatch('user/getInfo')
+
+          // generate accessible routes map based on roles
+          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+
+          // dynamically add accessible routes
+          router.addRoutes(accessRoutes)
+
+          // hack method to ensure that addRoutes is complete
+          // set the replace: true, so the navigation will not leave a history record
+          next({
+            ...to,
+            replace: true
+          })
+        } catch (error) {
+          // remove token and go to login page to re-login
+          await store.dispatch('user/resetToken')
+          next(`/login?redirect=${to.path}`)
+        }
+      }
+    }
+  } else {
+    /* has no token*/
+    if (whiteLists.indexOf(to.path) !== -1) {
+      // in the free login whitelist, go directly
+      next()
+    } else {
+      // other pages that do not have permission to access are redirected to the login page.
+      next(`/login?redirect=${to.path}`)
+    }
+  }
 })
-router.afterEach(() => {})
+router.afterEach(() => {
+  // todo
+})
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,9 @@
 const getters = {
   visitedViews: state => state.routerView.visitedViews,
-  cachedViews: state => state.routerView.cachedViews
+  cachedViews: state => state.routerView.cachedViews,
+  token: state => state.user.token,
+  name: state => state.user.name,
+  roles: state => state.user.roles,
+  permissionRoutes: state => state.permission.routes
 }
 export default getters
