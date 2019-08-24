4c144d242eaa1bfdecd33edfaacbc960cc14b0e0
JackieCheung <16275507@qq.com>
2019-08-24 22:57:18 +0800

    修正localStorage API

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,13 @@
 const TOKEN_KEY = 'Vue-Vant-Token'
 
 export function getToken() {
-  return window.localStorage.get(TOKEN_KEY)
+  return window.localStorage.getItem(TOKEN_KEY)
 }
 
 export function setToken(token) {
-  return window.localStorage.set(TOKEN_KEY, token)
+  return window.localStorage.setItem(TOKEN_KEY, token)
 }
 
 export function removeToken() {
-  return window.localStorage.remove(TOKEN_KEY)
+  return window.localStorage.removeItem(TOKEN_KEY)
 }
