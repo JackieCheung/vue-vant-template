d844799430f1b3a7e3a104ef8c8214089c94c982
Jackie Cheung <16275507@qq.com>
2021-01-19 11:51:34 +0800

    feat: update CalendarDemo

--- src/views/demo/calendar-demo/CalendarDemo.vue
+++ src/views/demo/calendar-demo/CalendarDemo.vue
@@ -5,7 +5,6 @@
       title="日历"
       :poppable="false"
       :show-confirm="false"
-      :default-date="new Date('2021-01-27')"
       :min-date="new Date('2021-01-01')"
       :max-date="new Date('2021-01-31')"
       :lazy-render="false"
@@ -19,6 +18,7 @@
 <script>
   import { getFinalStyle } from '@/utils/tools'
   import { scrollTo } from '@/utils/scroll-to'
+  const FOLDING_HEIGHT = '200px'
 
   export default {
     name: 'CalendarDemo',
@@ -26,19 +26,28 @@
       return {
         defaultHeight: '',
         height: '',
-        expand: true
+        expand: false
       }
     },
     mounted () {
-      this.height = this.defaultHeight = parseFloat(getFinalStyle(this.$el.querySelector('.van-calendar__days'), 'height')) + 48.58 * 2 + 32.8 + 'px'
+      this.defaultHeight = parseFloat(getFinalStyle(this.$el.querySelector('.van-calendar__days'), 'height')) + 48.58 * 2 + 32.8 + 'px'
+      this.height = FOLDING_HEIGHT
+      this.$nextTick(() => {
+        scrollTo(
+          this.$el.querySelector('.van-calendar__body'),
+          this.$el.querySelector('.van-calendar__body').scrollTop + this.$el.querySelector('.van-calendar__selected-day').getBoundingClientRect().top - 135,
+          200
+        )
+      })
     },
     methods: {
       click () {
         this.expand = !this.expand
-        this.height = this.expand ? this.defaultHeight : '200px'
         if (this.expand) {
+          this.height = this.defaultHeight
           this.$refs.calendar.scrollToDate(new Date())
         } else {
+          this.height = FOLDING_HEIGHT
           setTimeout(() => {
             // this.$el.querySelector('.van-calendar__selected-day').scrollIntoView({
             //   behavior: 'smooth'
