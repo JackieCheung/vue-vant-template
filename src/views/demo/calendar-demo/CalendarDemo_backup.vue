c80fbe85f6b84d7ce59163fb37e902af5a026120
Jackie Cheung <16275507@qq.com>
2021-01-22 01:19:38 +0800

    chore: update CalendarDemo_backup.vue

--- src/views/demo/calendar-demo/CalendarDemo_backup.vue
+++ src/views/demo/calendar-demo/CalendarDemo_backup.vue
@@ -14,7 +14,9 @@
       @select="handleSelect"
     />
     <br />
-    <van-button type="primary" @click="expand = !expand"> click</van-button>
+    <van-button type="primary" @click="expand = !expand">
+      click
+    </van-button>
   </div>
 </template>
 
@@ -52,12 +54,12 @@
       formatter (day) {
         const classArr = []
         if (day.type === 'disabled') classArr.push('d-none')
-        if (day.type === 'selected') classArr.push('selected')
         if (dayjs(day.date).isBefore(this.currentDate, 'month')) classArr.push('prev-month')
         if (dayjs(day.date).isSame(this.currentDate, 'month')) classArr.push('same-month')
         if (dayjs(day.date).isAfter(this.currentDate, 'month')) classArr.push('next-month')
         if (dayjs(day.date).isSame(this.selectedDate, 'week')) classArr.push('same-week')
         if (!this.expand && !classArr.includes('same-week')) classArr.push('fade-out')
+        if (day.type === 'selected') classArr.push('selected')
         day.className = classArr.join(' ')
         return day
       }
@@ -66,45 +68,45 @@
 </script>
 
 <style lang="scss" scoped>
-.van-calendar {
-  background-color: aliceblue;
+  .van-calendar {
+    background-color: aliceblue;
 
-  ::v-deep .van-calendar__days {
-    pointer-events: none;
+    ::v-deep .van-calendar__days {
+      pointer-events: none;
 
-    .van-calendar__day {
-      pointer-events: auto;
-      transition: all ease-in-out .4s, opacity ease-in-out .4s;
-
-      &.fade-out {
-        height: 0 !important;
-        opacity: 0 !important;
-      }
-
-      &.prev-month {
-        margin-bottom: -17.06667vw;
+      .van-calendar__day {
+        pointer-events: auto;
+        transition: all ease-in-out 4s;
 
         &.fade-out {
-          margin-bottom: 0;
+          height: 0 !important;
+          opacity: 0 !important;
         }
-      }
 
-      &.next-month {
-        margin-top: -17.06667vw;
+        &.prev-month {
+          margin-bottom: -17.06667vw;
 
-        &.fade-out {
-          margin-top: 0;
+          &.fade-out {
+            margin-bottom: 0;
+          }
         }
-      }
 
-      &.prev-month, &.next-month {
-        opacity: 0.5;
-      }
+        &.next-month {
+          margin-top: -17.06667vw;
 
-      &.selected {
-        opacity: 1;
+          &.fade-out {
+            margin-top: 0;
+          }
+        }
+
+        &.prev-month, &.next-month {
+          opacity: 0.5;
+
+          &.selected {
+            opacity: 1;
+          }
+        }
       }
     }
   }
-}
 </style>
